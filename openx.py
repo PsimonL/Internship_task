1b4e22dc9b65adf70c6b58432bfa158d55e3eccd
Szymon Rogowski <rogowski@student.agh.edu.pl>
2024-05-13 01:02:06 +0200

    openx.py test_openx.py linting advices fixed

--- openx.py
+++ openx.py
@@ -1,20 +1,32 @@
-from flask import Flask, request, jsonify
+"""
+This module defines a Flask application for converting Fahrenheit temperature to Celsius.
+"""
+
 import random
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-app_identifier = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890', k=10))
+APP_IDENTIFIER = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890', k=10))
+
 
 @app.route('/convert', methods=['POST'])
 def convert_fahrenheit_to_celsius():
+    """
+    Converts Fahrenheit temperature to Celsius.
+
+    Receives JSON data with Fahrenheit temperature in the request body.
+    Returns JSON response with the converted Celsius temperature.
+    """
     fahrenheit = request.json.get('fahrenheit')
-    
+
     if fahrenheit is None:
         return jsonify({'error': 'Fahrenheit temperature is required'}), 400
-    
-    celsius = (fahrenheit - 32) * 5.0/9.0
+
+    celsius = (fahrenheit - 32) * 5.0 / 9.0
     print("Request will be served.")
-    return jsonify({'celsius': celsius, 'app_identifier': app_identifier})
+    return jsonify({'celsius': celsius, 'app_identifier': APP_IDENTIFIER})
+
 
 if __name__ == '__main__':
     print("Server listening on port 8080...")
--- test_openx.py
+++ test_openx.py
@@ -1,28 +1,47 @@
+"""
+Unit tests for the openx module.
+"""
+
 import unittest
 from openx import app
 
 
 class TestOpenx(unittest.TestCase):
+    """
+    Test cases for the openx module.
+    """
+
     def setUp(self):
+        """
+        Set up the test client.
+        """
         self.app = app.test_client()
 
     def test_convert_fahrenheit_to_celsius_success(self):
+        """
+        Test conversion of Fahrenheit to Celsius with successful response.
+        """
         response = self.app.post('/convert', json={'fahrenheit': 32})
         data = response.get_json()
         self.assertEqual(response.status_code, 200)
         self.assertIn('celsius', data)
         self.assertIn('app_identifier', data)
 
-    def test_convert_32F_to_0C(self):
+    def test_convert_32f_to_0c(self):
+        """
+        Test conversion of 32°F to 0°C.
+        """
         response = self.app.post('/convert', json={'fahrenheit': 32})
         data = response.get_json()
         self.assertEqual(data['celsius'], 0)
 
-    def test_convert_212F_to_100C(self):
+    def test_convert_212f_to_100c(self):
+        """
+        Test conversion of 212°F to 100°C.
+        """
         response = self.app.post('/convert', json={'fahrenheit': 212})
         data = response.get_json()
         self.assertEqual(data['celsius'], 100)
-
 
 
 if __name__ == '__main__':
