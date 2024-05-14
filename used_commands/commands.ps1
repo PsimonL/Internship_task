2af4ebf597ee1b7059f988b9fe88ae2269f87be7
Szymon Rogowski <rogowski@student.agh.edu.pl>
2024-05-15 00:36:42 +0200

    minor fixes

--- used_commands/commands.ps1
+++ used_commands/commands.ps1
@@ -1,10 +1,10 @@
-Invoke-RestMethod -Uri http://localhost:5000/convert -Method Post -ContentType "application/json" -Body '{"fahrenheit": 32}'
-Invoke-RestMethod -Uri http://localhost:5000/probe -Method Get
+Invoke-RestMethod -Uri http://localhost:8080/convert -Method Post -ContentType "application/json" -Body '{"fahrenheit": 32}'
+Invoke-RestMethod -Uri http://localhost:8080/probe -Method Get
 
 docker build -t openx_intern_task .
 docker build -t openx_intern_task -f Dockerfile-go .
 
-docker run -p 5000:5000 openx_intern_task
+docker run -p 8080:8080 openx_intern_task
 
 helm install openx-chart ./openx-flask-api
 
