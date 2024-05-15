a67f4a33f26620be7052a140183a1bd39522725f
Szymon Rogowski <rogowski@student.agh.edu.pl>
2024-05-15 23:42:43 +0200

    commands update

--- used_commands/commands.ps1
+++ used_commands/commands.ps1
@@ -1,11 +1,33 @@
+# ============================================================================================================================================================
+# REQUESTS TO TEST
 Invoke-RestMethod -Uri http://localhost:8080/convert -Method Post -ContentType "application/json" -Body '{"fahrenheit": 32}'
+# Example minikube tunnel for service api
+Invoke-RestMethod -Uri http://127.0.0.1:55343/convert -Method Post -ContentType "application/json" -Body '{"fahrenheit": 32}'
 Invoke-RestMethod -Uri http://localhost:8080/probe -Method Get
+# kubectl exec openx-api-684b578f57-nhll5 -- curl -X POST http://localhost:5000/convert -H "Content-Type: application/json" -d '{"fahrenheit": 32}'
+# ============================================================================================================================================================
 
+# ============================================================================================================================================================
+# API STEPS
 docker build -t openx_intern_task .
 docker build -t openx_intern_task -f Dockerfile-go .
-
-docker run -p 8080:8080 openx_intern_task
-
-helm install openx-chart ./openx-flask-api
-
+docker run -p 8080:8080 srpl/openx_intern_task:1.0.0
+helm install openx-chart ./api
+# Very important cause of minikube internal tunneling
 minikube service openx-api
+# ============================================================================================================================================================
+
+# ============================================================================================================================================================
+# LOCUST STEPS
+locust -f locustfile.py --headless --host=http://localhost:8080
+docker build -t openx_intern_task_locust -f Dockerfile-locust .
+docker tag openx_intern_task_locust:latest srpl/openx_intern_task_locust:1.0.0
+docker login
+docker push openx_intern_task_locust:1.0.0
+# ============================================================================================================================================================
+
+# ============================================================================================================================================================
+# TENSORFLOW SERVING STEPS
+docker pull tensorflow/serving
+docker run -t --rm -p 8501:8501 -v "/model/saved_model" -e MODEL_NAME=saved_model tensorflow/serving 
+# ============================================================================================================================================================
--- used_commands/commands.sh
+++ used_commands/commands.sh
@@ -1,10 +1,36 @@
 #! /bin/bash
-curl -X POST http://localhost:5000/convert -H "Content-Type: application/json" -d '{"fahrenheit": 32}'
 
-kubectl exec openx-api-684b578f57-nhll5 -- curl -X POST http://localhost:5000/convert -H "Content-Type: application/json" -d '{"fahrenheit": 32}'
+# ============================================================================================================================================================
+# REQUESTS TO TEST
+curl -X POST -H "Content-Type: application/json" -d '{"fahrenheit": 32}' http://localhost:8080/convert
+# Example minikube tunnel for service api
+curl -X POST -H "Content-Type: application/json" -d '{"fahrenheit": 32}' http://127.0.0.1:55343/convert
+curl -X GET http://localhost:8080/probe
+# kubectl exec openx-api-684b578f57-nhll5 -- curl -X POST -H "Content-Type: application/json" -d '{"fahrenheit": 32}' http://localhost:5000/convert
+# ============================================================================================================================================================
 
+# ============================================================================================================================================================
+# API STEPS
 docker build -t openx_intern_task .
+docker build -t openx_intern_task -f Dockerfile-go .
+docker run -p 8080:8080 srpl/openx_intern_task:1.0.0
+helm install openx-chart ./api
+# Very important cause of minikube internal tunneling
+minikube service openx-api
+# ============================================================================================================================================================
 
-docker run -p 5000:5000 openx_intern_task
+# ============================================================================================================================================================
+# LOCUST STEPS
+locust -f locustfile.py --headless --host=http://localhost:8080
+docker build -t openx_intern_task_locust -f Dockerfile-locust .
+docker tag openx_intern_task_locust:latest srpl/openx_intern_task_locust:1.0.0
+docker login
+docker push openx_intern_task_locust:1.0.0
+# ============================================================================================================================================================
 
-helm install openx-chart ./openx-flask-api
+# ============================================================================================================================================================
+# TENSORFLOW SERVING STEPS
+docker pull tensorflow/serving
+docker run -t --rm -p 8501:8501 -v "/model/saved_model" -e MODEL_NAME=saved_model tensorflow/serving 
+# ============================================================================================================================================================
+
