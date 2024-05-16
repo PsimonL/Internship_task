7af80a406de1a8faafd9800f45f71fc54931f956
Szymon Rogowski <rogowski@student.agh.edu.pl>
2024-05-16 21:57:31 +0200

    locust description

--- locustfile.py
+++ locustfile.py
@@ -1,10 +1,21 @@
+"""
+This file contains a Locust performance testing script for simulating user behavior of converting temperature units.
+"""
+
+
 from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
-    wait_time = between(1, 3) 
+    """
+    A Locust User class representing a simulated user performing api calls for temperature conversion tasks.
+    """
+    wait_time = between(1, 5)
 
     @task
     def convert_temperature(self):
+        """
+        Task method representing the user converting temperature from Fahrenheit to Celsius.
+        """
         response = self.client.post("/convert", json={"fahrenheit": 32})
 
         if response.status_code == 200:
